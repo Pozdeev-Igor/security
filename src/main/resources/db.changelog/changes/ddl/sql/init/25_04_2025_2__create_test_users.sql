27d0bc09cbb140e69a2f1df6284c78a1239b7d8b
Igor <Sketch_90@mail.ru>
2025-04-25 16:37:34 +0300

    [SECURITY]-03- fix db

--- src/main/resources/db.changelog/changes/ddl/sql/init/25_04_2025_2__create_test_users.sql
+++ src/main/resources/db.changelog/changes/ddl/sql/init/25_04_2025_2__create_test_users.sql
@@ -1,17 +1,17 @@
-insert into users (id, cohort_start_date, email, name, password, phone_number, username, rate, count, avatar)
-values  ('218cf602-d846-11ed-afa1-0242ac120002', current_date, 'test@mail.com', 'Admin Adminovich', '$2a$12$qlHfW5DyWsRoutTBkSy7/.QjVYNsHOSi4a1yrnUgUymxoHzFgKmNK', '79788563182', 'admin', 5.0, 1, ''),
-        ('218cf8fa-d846-11ed-afa1-0242ac120002', current_date, 'non-test@mail.com', 'Vasya RND', '$2a$12$qlHfW5DyWsRoutTBkSy7/.QjVYNsHOSi4a1yrnUgUymxoHzFgKmNK', '79788563183', 'user1', 0.0, 0, ''),
-        ('218cf9fe-d846-11ed-afa1-0242ac120002', current_date, 'super-test@mail.com', 'Mary Bibika', '$2a$12$qlHfW5DyWsRoutTBkSy7/.QjVYNsHOSi4a1yrnUgUymxoHzFgKmNK', '79788563184', 'mary', 0.0, 0, ''),
-        ('218cfaf8-d846-11ed-afa1-0242ac120002', current_date, 'mega-test@mail.com', 'Danila Bagrov', '$2a$12$qlHfW5DyWsRoutTBkSy7/.QjVYNsHOSi4a1yrnUgUymxoHzFgKmNK', '79788563185', 'danila', 0.0, 0, '');
+insert into users (id, cohort_start_date, email, name, password, phone_number, username, rate, count, avatar, enabled)
+values  ('218cf602-d846-11ed-afa1-0242ac120002', current_date, 'test@mail.com', 'Admin Adminovich', '$2a$12$qlHfW5DyWsRoutTBkSy7/.QjVYNsHOSi4a1yrnUgUymxoHzFgKmNK', '79788563182', 'admin', 5.0, 1, '', true),
+        ('218cf8fa-d846-11ed-afa1-0242ac120002', current_date, 'non-test@mail.com', 'Vasya RND', '$2a$12$qlHfW5DyWsRoutTBkSy7/.QjVYNsHOSi4a1yrnUgUymxoHzFgKmNK', '79788563183', 'user1', 0.0, 0, '', true),
+        ('218cf9fe-d846-11ed-afa1-0242ac120002', current_date, 'super-test@mail.com', 'Mary Bibika', '$2a$12$qlHfW5DyWsRoutTBkSy7/.QjVYNsHOSi4a1yrnUgUymxoHzFgKmNK', '79788563184', 'mary', 0.0, 0, '', true),
+        ('218cfaf8-d846-11ed-afa1-0242ac120002', current_date, 'mega-test@mail.com', 'Danila Bagrov', '$2a$12$qlHfW5DyWsRoutTBkSy7/.QjVYNsHOSi4a1yrnUgUymxoHzFgKmNK', '79788563185', 'danila', 0.0, 0, '', true);
 
-insert into authorities (id, authority_name, user_id)
-values  ('d94807d0-d893-11ed-afa1-0242ac120002', 'ROLE_ADMIN', '218cf602-d846-11ed-afa1-0242ac120002'),
-        ('d9480b54-d893-11ed-afa1-0242ac120002', 'ROLE_CONSUMER', '218cf602-d846-11ed-afa1-0242ac120002'),
-        ('d9480cf8-d893-11ed-afa1-0242ac120002', 'ROLE_PRODUCER', '218cf602-d846-11ed-afa1-0242ac120002'),
-        ('d9480e60-d893-11ed-afa1-0242ac120002', 'ROLE_PRODUCER', '218cf8fa-d846-11ed-afa1-0242ac120002'),
-        ('d94811f8-d893-11ed-afa1-0242ac120002', 'ROLE_CONSUMER', '218cf8fa-d846-11ed-afa1-0242ac120002'),
-        ('d94813c4-d893-11ed-afa1-0242ac120002', 'ROLE_CONSUMER', '218cf9fe-d846-11ed-afa1-0242ac120002'),
-        ('d9481540-d893-11ed-afa1-0242ac120002', 'ROLE_CONSUMER', '218cfaf8-d846-11ed-afa1-0242ac120002');
+insert into authorities (authority, username)
+values  ('ROLE_ADMIN', 'admin'),
+        ('ROLE_CONSUMER', 'admin'),
+        ('ROLE_PRODUCER', 'admin'),
+        ('ROLE_PRODUCER', 'user1'),
+        ('ROLE_CONSUMER', 'user1'),
+        ('ROLE_CONSUMER', 'mary'),
+        ('ROLE_CONSUMER', 'danila');
 
 
 insert into category (id, name)
